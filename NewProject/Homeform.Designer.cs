c3eb0c639ac8807d35b25552c7505f9a45ec0443
Mottak <Mottak@MOTTAK-PC>
2020-04-06 18:22:13 +0200

    Add project files.

---
+++ NewProject/Homeform.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace NewProject
+{
+    partial class HomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Toppanel = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.Toppanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Toppanel
+            // 
+            this.Toppanel.BackColor = System.Drawing.Color.DodgerBlue;
+            this.Toppanel.Controls.Add(this.button3);
+            this.Toppanel.Controls.Add(this.button1);
+            this.Toppanel.Controls.Add(this.CloseButton);
+            this.Toppanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Toppanel.Location = new System.Drawing.Point(0, 0);
+            this.Toppanel.Name = "Toppanel";
+            this.Toppanel.Size = new System.Drawing.Size(1067, 27);
+            this.Toppanel.TabIndex = 1;
+            this.Toppanel.DragDrop += new System.Windows.Forms.DragEventHandler(this.Toppanel_DragDrop);
+            this.Toppanel.Paint += new System.Windows.Forms.PaintEventHandler(this.Toppanel_Paint);
+            this.Toppanel.DoubleClick += new System.EventHandler(this.Toppanel_DoubleClick);
+            this.Toppanel.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Toppanel_MouseDoubleClick);
+            this.Toppanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Toppanel_MouseDown);
+            this.Toppanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Toppanel_MouseMove);
+            this.Toppanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Toppanel_MouseUp);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button3.Location = new System.Drawing.Point(833, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(78, 27);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Minimize";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.MinimizeButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Location = new System.Drawing.Point(911, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Maximize";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.MaximizeButton_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseButton.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.CloseButton.Location = new System.Drawing.Point(989, 0);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(78, 27);
+            this.CloseButton.TabIndex = 0;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // HomeForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(1067, 650);
+            this.Controls.Add(this.Toppanel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "HomeForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PatientInformation";
+            this.Load += new System.EventHandler(this.PatientInformation_Load);
+            this.Toppanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel Toppanel;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+    }
+}
